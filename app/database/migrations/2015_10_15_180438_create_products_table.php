b79dc3aa8faffe19abaea2f3f7d25b48817fcb05
Tom Young <tom.young.satx@gmail.com>
2015-10-28 10:21:30 -0500

    branch

--- app/database/migrations/2015_10_15_180438_create_products_table.php
+++ app/database/migrations/2015_10_15_180438_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration {
 		Schema::create('products', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('category');
+			$table->string('brand');
+			$table->text('description');
+			$table->string('img');
 			$table->timestamps();
 		});
 	}
